e82fdbec91fd64a60678bddc1e294132f8abd0a7
Dharuan Luigi <dharuanluigi@gmail.com>
2022-09-26 21:58:31 -0300

    Added: dinamic style when task is done

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,19 +1,31 @@
+import { useState } from "react";
 import * as Checkbox from "@radix-ui/react-checkbox";
 
 import { Check, Trash } from "phosphor-react";
 import styles from "./Task.module.css";
 
 export function Task() {
+  const [isTaskDone, setIsTaskDone] = useState<boolean>(false);
+
+  function handleTaskIsDone(isChecked: boolean) {
+    setIsTaskDone(isChecked);
+  }
+
   return (
     <div className={styles.taskContainer}>
       <div className={styles.task}>
-        <Checkbox.Root className={styles.checked}>
+        <Checkbox.Root
+          className={isTaskDone ? styles.checked : styles.unChecked}
+          onCheckedChange={handleTaskIsDone}
+        >
           <Checkbox.Indicator className={styles.centerizeCheck}>
             <Check weight="bold" />
           </Checkbox.Indicator>
         </Checkbox.Root>
         <div className={styles.taskContent}>
-          <span className={styles.checked_text}>
+          <span
+            className={isTaskDone ? styles.checked_text : styles.unChecked_text}
+          >
             Lorem ipsum dolor sit amet consectetur.
           </span>
         </div>
