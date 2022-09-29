5d75fa6f70a551e10338e51572367df1bcd3d2d7
Dharuan Luigi <dharuanluigi@gmail.com>
2022-09-28 22:42:09 -0300

    Changed: model to get description property

--- src/models/task.model.tsx
+++ src/models/task.model.tsx
@@ -2,10 +2,12 @@ export class TaskModel {
   constructor(
     public id: number,
     public content: string,
-    public isDone = false
+    public isDone = false,
+    public description?: string
   ) {
     this.id = id;
     this.content = content;
     this.isDone = isDone;
+    this.description = description;
   }
 }
