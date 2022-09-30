779187c4e31824e514c4371e1990afe424079d5d
Dharuan Luigi <dharuanluigi@gmail.com>
2022-09-29 23:18:42 -0300

    Added: sort to done and not done tasks

--- src/components/ViewTask.tsx
+++ src/components/ViewTask.tsx
@@ -1,3 +1,6 @@
+import { CaretDown, CaretUp } from "phosphor-react";
+import { useState } from "react";
+import { ESortTasks } from "../App";
 import { EmptyTaskView } from "./EmptyTaskView";
 import { Task, TaskProps } from "./Task";
 
@@ -8,6 +11,7 @@ interface ViewTaskProps {
   onTaskCompleted: (taskId: number, isChecked: boolean) => void;
   onDeleteTask: (taskId: number) => void;
   onAddDescription: (task: TaskProps) => void;
+  onSortTasks: (orientation: ESortTasks) => void;
 }
 
 export function ViewTask({
@@ -15,21 +19,57 @@ export function ViewTask({
   onTaskCompleted,
   onDeleteTask,
   onAddDescription,
+  onSortTasks,
 }: ViewTaskProps) {
+  const [isDoneTasksSortClick, setIsDoneTasksSortClick] = useState(false);
+  const [isNotDoneTasksSortClick, setIsNotDoneTasksSortClick] = useState(true);
+
   function calcHowManyTasksIsDone() {
     let total = tasks.filter((task) => task.isDone);
     return total.length;
+  }
+
+  function handleClickSortDoneTasks() {
+    onSortTasks(ESortTasks.IsDone);
+    setIsDoneTasksSortClick((value) => !value);
+    setIsNotDoneTasksSortClick((value) => !value);
+  }
+
+  function handleClickSortIsNotDoneTasks() {
+    onSortTasks(ESortTasks.IsNotDone);
+    setIsNotDoneTasksSortClick((value) => !value);
+    setIsDoneTasksSortClick((value) => !value);
   }
 
   return (
     <div className={styles.taskViewContainer}>
       <div className={styles.taskHeader}>
         <div>
-          <span className={styles.totalCreatedTaskTitle}>Tarefas criadas</span>
+          <span
+            className={styles.totalCreatedTaskTitle}
+            onClick={handleClickSortIsNotDoneTasks}
+          >
+            {isNotDoneTasksSortClick ? (
+              <CaretUp weight="bold" />
+            ) : (
+              <CaretDown weight="bold" />
+            )}{" "}
+            Tarefas criadas
+          </span>
           <span className={styles.counterTasks}>{tasks.length}</span>
         </div>
         <div>
-          <span className={styles.totalCompletedTaskTitle}>Concluídas</span>
+          <span
+            className={styles.totalCompletedTaskTitle}
+            onClick={handleClickSortDoneTasks}
+          >
+            {isDoneTasksSortClick ? (
+              <CaretUp weight="bold" />
+            ) : (
+              <CaretDown weight="bold" />
+            )}{" "}
+            Concluídas
+          </span>
           <span className={styles.counterTasks}>
             {calcHowManyTasksIsDone()} de {tasks.length}
           </span>
